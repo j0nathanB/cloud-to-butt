e363a2ce30298d7147820b71d9fe8000db87fa74
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2018-07-23 22:26:52 -0700

    Allow user to set options in Chrome

--- content_script.js
+++ content_script.js
@@ -1,4 +1,8 @@
-walk(document.body);
+var extensionOption = null;
+chrome.storage.sync.get('userOption', function(result) {
+	extensionOption = result.userOption
+	walk(document.body);
+});
 
 function walk(node) {
 	// I stole this function from here:
@@ -24,18 +28,33 @@ function walk(node) {
 			break;
 
 		case 3: // Text node
-			handleText(node);
+			handleText(node, extensionOption);
 			break;
 	}
 }
 
-function handleText(textNode) {
+function handleText(textNode, userOption) {
 	var v = textNode.nodeValue;
+	console.log('userOption: ', userOption);
+	
+	if (userOption === 'toLatino') {
+		v = v.replace(/\bLatinx\b/g, "Latino");
+		v = v.replace(/\blatinx\b/g, "latino");
+		v = v.replace(/\bLatinxs\b/g, "Latinos");
+		v = v.replace(/\blatinxs\b/g, "latinos");
+	}
 
-	v = v.replace(/\bLatinx\b/g, "Latino");
-	v = v.replace(/\blatinx\b/g, "latino");
-	v = v.replace(/\bLatinxs\b/g, "Latinos");
-	v = v.replace(/\blatinxs\b/g, "latinos");
+	if (userOption === 'toLatinx') {
+		v = v.replace(/\bLatino\b/g, "Latinx");
+		v = v.replace(/\blatino\b/g, "latinx");
+		v = v.replace(/\bLatinos\b/g, "Latinxs");
+		v = v.replace(/\blatinos\b/g, "latinxs");
+
+		v = v.replace(/\bLatina\b/g, "Latinx");
+		v = v.replace(/\blatina\b/g, "latinx");
+		v = v.replace(/\bLatinas\b/g, "Latinxs");
+		v = v.replace(/\blatinas\b/g, "latinxs");
+	}
 	
 	textNode.nodeValue = v;
 }
